b61b559b4a1ae9972829b111dc1dd89e9b97d1a0
Ilayoub <jrindou.ayoub@gmail.com>
2023-06-19 16:51:39 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+int n1, n2;
+
+	for (n1 = 48; n1 <= 56; n1++)
+	{
+		for (n2 = 49; 2 <= 57; n2++)
+		{
+			if (n2 > n1)
+			{
+				putchar(n1);
+				putchar(n2);
+				if (n1 != 56 || n2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
