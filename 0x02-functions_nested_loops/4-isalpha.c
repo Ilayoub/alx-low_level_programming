ff442c00d2b6d6456a9892ae11fb9f4268410d3c
Ilayoub <jrindou.ayoub@gmail.com>
2023-06-20 15:37:31 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - check for alphabetic character
- * c is the character to be checked
+ * @c: is the character to be checked
  * Return: 1 if c is a letter, 0 otherwise;
  */
 int _isalpha(int c)
