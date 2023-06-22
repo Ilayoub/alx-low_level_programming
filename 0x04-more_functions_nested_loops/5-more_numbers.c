e16bdd8c6f6cc4c5ffe620b5a374d58c38efbda1
Ilayoub <jrindou.ayoub@gmail.com>
2023-06-22 15:40:03 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - prints 10 times of the numbers from 0 to 14
+ * more_numbers - prints 10 times of the numbers from 0 to 14
  * must be followed by a new line
  * Return: Always 0.
  */
