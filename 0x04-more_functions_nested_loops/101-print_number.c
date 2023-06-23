b1cb303547428a5d6633f96cb5392cbeb9870c67
Ilayoub <jrindou.ayoub@gmail.com>
2023-06-22 18:33:40 -0700

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints an integer
+ * @n : is a variable
+ * Return: always 0 (Success)
+ */
+
+void print_number(int n)
+
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		putchar('-');
+	} else
+	{
+		m = n;
+	}
+
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+
+	putchar((m % 10) + '0');
+}
